ae596b742c9dc3fd960c84f98aee42335464247c
Jorge Kleber <jorge_kleber@outlook.com>
2024-05-02 12:48:14 +0100

    teste com ViewModel.

---
+++ app/src/main/java/com/example/fake_social_network/viewmodels/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.fake_social_network.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+}
--- app/src/main/java/com/example/fake_social_network/views/MainActivity.kt
+++ app/src/main/java/com/example/fake_social_network/views/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.fake_social_network
+package com.example.fake_social_network.views
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.fake_social_network.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
